5a34177023d38c7084d9e27e57947a7c8577e407
magomedt149 <magomedt149@gmail.com>
2026-09-04 04:05:47 -0700

    Invalidate iPhone cache for Tumsoev audio fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-v52-tumsoev-t-tap-20260904';
+const CACHE = 'nova-v53-tumsoev-audio-20260904';
 const API_CACHE = 'nova-api-economy-v2';
 const METERED_NETLIFY_HOST = /(^|\\.)netlify\\.app$/i.test(self.location.hostname);
 const YOUTUBE_TTL_MS = 24 * 60 * 60 * 1000;
