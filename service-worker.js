45337f60cd56b57dd14078f483c99498e9cbaae7
magomedt149 <magomedt149@gmail.com>
2026-09-04 13:08:08 -0700

    Refresh NOVA 27.5 PWA cache after runtime fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-v62-blender-camera-bridge-20260904';
+const CACHE = 'nova-v63-blender-camera-bridge-20260904';
 const API_CACHE = 'nova-api-economy-v2';
 const METERED_NETLIFY_HOST = /(^|\\.)netlify\\.app$/i.test(self.location.hostname);
 const YOUTUBE_TTL_MS = 24 * 60 * 60 * 1000;
