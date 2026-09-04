8b995eb4c4285b2962f97a3fa10b2670b3953adf
magomedt149 <magomedt149@gmail.com>
2026-09-04 03:22:36 -0700

    Invalidate old NOVA voice-switch cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-v47-irina-denis-switch-20260904';
+const CACHE = 'nova-v48-irina-denis-live-switch-20260904';
 const API_CACHE = 'nova-api-economy-v2';
 const METERED_NETLIFY_HOST = /(^|\\.)netlify\\.app$/i.test(self.location.hostname);
 const YOUTUBE_TTL_MS = 24 * 60 * 60 * 1000;
