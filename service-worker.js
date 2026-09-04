54680efc3b280cc93795eb459561736c68ddcbe5
magomedt149 <magomedt149@gmail.com>
2026-09-04 12:55:34 -0700

    Refresh PWA cache for true orbit fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-v60-3d-director-20260904';
+const CACHE = 'nova-v61-true-blender-orbit-20260904';
 const API_CACHE = 'nova-api-economy-v2';
 const METERED_NETLIFY_HOST = /(^|\\.)netlify\\.app$/i.test(self.location.hostname);
 const YOUTUBE_TTL_MS = 24 * 60 * 60 * 1000;
