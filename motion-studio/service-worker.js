a5f702cf51f58d74b8b1d4958534ad6505b2b85a
magomedt149 <magomedt149@gmail.com>
2026-09-04 18:20:39 -0700

    Bump Motion Studio cache for human motion update

--- motion-studio/service-worker.js
+++ motion-studio/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'tumsoev-motion-vfx-studio-v22-true-orbit';
+const CACHE = 'tumsoev-motion-vfx-studio-v23-human-motion-control';
 const ASSETS = [
   '/motion-studio/',
   '/motion-studio/index.html',
