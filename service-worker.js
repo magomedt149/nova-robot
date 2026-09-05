662b37c290daf1a5708c765dba37444f99b5796a
magomedt149 <magomedt149@gmail.com>
2026-09-04 23:13:04 -0700

    Refresh NOVA cache for Autocalls calling

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-v64-motion-diagnostics-v24-20260904';
+const CACHE = 'nova-v65-autocalls-confirm-v1-20260904';
 const API_CACHE = 'nova-api-economy-v2';
 const METERED_NETLIFY_HOST = /(^|\\.)netlify\\.app$/i.test(self.location.hostname);
 const YOUTUBE_TTL_MS = 24 * 60 * 60 * 1000;
