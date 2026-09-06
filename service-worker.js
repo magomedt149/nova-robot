6ac7709e14346f40a38f0e48d974d91cfd7ca3d0
magomedt149 <magomedt149@gmail.com>
2026-09-05 17:14:43 -0700

    Refresh NOVA cache for FFmpeg media commands

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-v70-free-call-easy-check-v1-20260904';
+const CACHE = 'nova-v71-media-command-ffmpeg-v1-20260905';
 const API_CACHE = 'nova-api-economy-v2';
 const METERED_NETLIFY_HOST = /(^|\\.)netlify\\.app$/i.test(self.location.hostname);
 const YOUTUBE_TTL_MS = 24 * 60 * 60 * 1000;
