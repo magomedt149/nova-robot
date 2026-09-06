4a30331a57c0830bde6a0d8afa50cfb2b4b412e9
magomedt149 <magomedt149@gmail.com>
2026-09-05 17:44:55 -0700

    Refresh NOVA PWA cache for chord music commands

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-v75-multitrack-voice-v2-20260905';
+const CACHE = 'nova-v76-free-chord-music-v1-20260905';
 const API_CACHE = 'nova-api-economy-v2';
 const METERED_NETLIFY_HOST = /(^|\\.)netlify\\.app$/i.test(self.location.hostname);
 const YOUTUBE_TTL_MS = 24 * 60 * 60 * 1000;
