ba3a26c53ac5d00d1c9e576da6dc487e569d7495
magomedt149 <magomedt149@gmail.com>
2026-09-06 16:52:55 -0700

    chore: refresh PWA cache for automatic camera failover

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-v84-glasses-connect-20260906';
+const CACHE = 'nova-v85-auto-camera-failover-20260906';
 const API_CACHE = 'nova-api-economy-v2';
 const METERED_NETLIFY_HOST = /(^|\\.)netlify\\.app$/i.test(self.location.hostname);
 const YOUTUBE_TTL_MS = 24 * 60 * 60 * 1000;
