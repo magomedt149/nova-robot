384b71fd5fa5c8a4bfde31e34a9789daf68d5557
magomedt149 <magomedt149@gmail.com>
2026-09-06 16:57:25 -0700

    chore: bump NOVA app to 27.20.0

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '27.19.0';
+  const VERSION = '27.20.0';
   const $ = (selector) => document.querySelector(selector);
   const $$ = (selector) => [...document.querySelectorAll(selector)];
 
