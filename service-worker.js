ff81892f87acb75f34ac190dbc1e3ab13cc5e9b0
magomedt149 <magomedt149@gmail.com>
2026-09-06 16:49:18 -0700

    chore: refresh PWA cache for glasses connection screen

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-v83-scene-live-20260906';
+const CACHE = 'nova-v84-glasses-connect-20260906';
 const API_CACHE = 'nova-api-economy-v2';
 const METERED_NETLIFY_HOST = /(^|\\.)netlify\\.app$/i.test(self.location.hostname);
 const YOUTUBE_TTL_MS = 24 * 60 * 60 * 1000;
