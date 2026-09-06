9b996ad9db066730b41bb218eeb3095673f71b31
magomedt149 <magomedt149@gmail.com>
2026-09-06 15:38:14 -0700

    Synchronize NOVA app runtime version to 27.13.0

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '27.6.0';
+  const VERSION = '27.13.0';
   const $ = (selector) => document.querySelector(selector);
   const $$ = (selector) => [...document.querySelectorAll(selector)];
 
