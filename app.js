203ab15b5c4c5025175f2a73d8167f69f4b5749c
magomedt149 <magomedt149@gmail.com>
2026-09-08 02:54:42 -0700

    fix: synchronize NOVA runtime version for MCP release

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '27.26.0';
+  const VERSION = '27.27.0';
   const $ = (selector) => document.querySelector(selector);
   const $$ = (selector) => [...document.querySelectorAll(selector)];
 
